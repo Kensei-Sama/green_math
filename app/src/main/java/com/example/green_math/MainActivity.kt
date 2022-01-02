6e652589daa8cf92366dd97c9927cb91c6f5e664
Kensei-Sama <52254166+Kensei-Sama@users.noreply.github.com>
2022-01-02 23:01:01 +0100

    spinner

--- app/src/main/java/com/example/green_math/MainActivity.kt
+++ app/src/main/java/com/example/green_math/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.green_math
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val classes = resources.getStringArray(R.array.classesArray)
+
     }
 }
